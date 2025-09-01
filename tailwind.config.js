ab7edf84e8119486bd1d42f88b901cce27bcc9a0
activeitplz-crypto <activeitplz@gmail.com>
2025-09-01 18:28:54 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#16a34a" // green theme
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+}
